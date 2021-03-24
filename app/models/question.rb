d22298700a5fe2942e883153af995f239e66791c
Anson Lam <anson.lam94@gmail.com>
2021-03-24 13:11:55 -0700

    Add Question model and migrations

---
+++ app/models/question.rb
@@ -0,0 +1,7 @@
+class Question < ApplicationRecord
+  # Active Record is known as an ORM, an Object Relational Mapper
+  # We use objects to represent tables and data from our database.
+  # This class "Question" represents the "questions" table.
+  # Instances of the Question class represent a row of data in 
+  # the "questions" table.
+end
