832a0942ec647281e133590b7bf3aaf120b5b24c
Anson Lam <anson.lam94@gmail.com>
2021-03-29 20:34:32 -0700

    Add crud to questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  post('/contact', { to: "contact#create" })
+  post("/contact", { to: "contact#create" })
   get("/contact/new", { to: "contact#new" })
 
   # This defines a route definition that says when a GET request to "/" is made, handle it in the 
@@ -10,4 +10,29 @@ Rails.application.routes.draw do
   # for you, but we can rename it using the "as" option. All similar HTTP verbs have an associated
   # method named after the verb.
   get("/", { to: "welcome#index", as: "root" })
+
+  # Renders a form to create a new question
+  get("/questions/new", to: "questions#new", as: "new_question")
+
+  # Create a question (submitting the new question form)
+  post("/questions", to: "questions#create")
+
+  # Render a question show page
+  # Helper method would use the id or instance as an argument
+  # question_path(<id>) or question_path(instance)
+  get("/questions/:id", to: "questions#show", as: "question")
+
+  # Render a list of all questions
+  get("/questions", to: "questions#index")
+
+  # Render a form to edit an existing question
+  # Helper method would use the id or instance as an argument
+  # edit_question_path(<id>) or edit_question_path(instance)
+  get("/questions/:id/edit", to: "questions#edit", as: "edit_question")
+
+  # Update a question in the database (submission of the edit form)
+  patch("/questions/:id", to: "questions#update")
+
+  # Delete a question
+  delete("questions/:id", to: "questions#destroy")
 end
