82fc6133db849260dcb6a34967b2632955c5eb3f
helgakern <helgakern@gmail.com>
2021-05-17 11:09:09 -0700

    added user and answers to the question show page

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -5,6 +5,20 @@ class QuestionSerializer < ActiveModel::Serializer
     object.likes.count
   end
 
+  belongs_to :user, key: :author
+  class UserSerializer < ActiveModel::Serializer
+    attributes :id, :first_name, :last_name, :full_name
+  end
+
+  has_many :answers
+
+  class AnswerSerializer < ActiveModel::Serializer
+    attributes :id, :body, :created_at, :updated_at, :author_full_name
+      def author_full_name
+        object.user&.full_name
+      end
+  end
+
 end
 
 
